8c2b07556d01bf4029e80bf986180fdb81e0cdb0
Anh-Thu Phan <aphan@ufl.edu>
2021-02-13 19:44:36 -0700

    Make adjustments to sticky navbar jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,49 @@
+//TOOLTIP
+
 $(function () {
   $('[data-bs-toggle="tooltip"]').tooltip()
 })
-
-//HEADER PARTICLES
-
-particlesJS("particles-js", {"particles":{"number":{"value":30,"density":{"enable":true,"value_area":800}},"color":{"value":"#2c2c2c"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000"},"polygon":{"nb_sides":6},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.3,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":100,"random":true,"anim":{"enable":true,"speed":10,"size_min":40,"sync":false}},"line_linked":{"enable":false,"distance":200,"color":"#ffffff","opacity":1,"width":2},"move":{"enable":true,"speed":8,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});;
 
 
 
 
 //STICKY NAVBAR
 
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
+var stickyNavTop = $('.navbar').offset().top;
 
-// // Get the navbar
-// var navbar = document.getElementById("navbar");
+function scrolling() {
+  doc = $(document).height()
+  hidingtop = $('.hiding').offset().top;
+  hidingbottom = $('.hiding').position().top + $('.hiding').outerHeight(true)
+  if ($(window).scrollTop() > hidingtop && $(window).scrollTop() < hidingbottom) {
+    $('.navbar').removeClass('sticky');
+  }
+}
 
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
+var stickyNav = function () {
+  var scrollTop = $(window).scrollTop();
 
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+  if (scrollTop > stickyNavTop) {
+    $('.navbar').addClass('sticky');
+  } else {
+    $('.navbar').removeClass('sticky');
+  }
+};
 
-// document.addEventListener ("scroll",
-//   function () {
-    
-//   }, false
-// );
+stickyNav();
+
+$(window).scroll(function () {
+  stickyNav();
+
+  scrolling()
+});
+
+
+
+
+//HOME PARTICLES
+
+particlesJS("particles-js", { "particles": { "number": { "value": 30, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#2c2c2c" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000" }, "polygon": { "nb_sides": 6 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.3, "random": true, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 100, "random": true, "anim": { "enable": true, "speed": 10, "size_min": 40, "sync": false } }, "line_linked": { "enable": false, "distance": 200, "color": "#ffffff", "opacity": 1, "width": 2 }, "move": { "enable": true, "speed": 8, "direction": "none", "random": true, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": false, "mode": "grab" }, "onclick": { "enable": false, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });;
 
 
 
@@ -41,34 +51,34 @@ particlesJS("particles-js", {"particles":{"number":{"value":30,"density":{"enabl
 //CONSTANTS TO REMOVE ABOUT HEADER (getRid2 is to reverse)
 
 const aboutHeader = document.querySelector("#wrapper h2");
-const getRid = function () {document.querySelector("#wrapper").removeChild(aboutHeader);}
+const getRid = function () { document.querySelector("#wrapper").removeChild(aboutHeader); }
 
-const getRid2 = function () {article.removeChild(document.querySelector("#aboutArticle h2"));}
+const getRid2 = function () { article.removeChild(document.querySelector("#aboutArticle h2")); }
 
 //CONSTANTS TO PREPEND ABOUT HEADER TO ABOUT ARTICLE (addHeader2 is to reverse)
 const article = document.querySelector("#aboutArticle");
-const addHeader = function () {article.insertBefore(aboutHeader, article.firstChild);}
+const addHeader = function () { article.insertBefore(aboutHeader, article.firstChild); }
 
-const addHeader2 = function () {document.querySelector("#wrapper").insertBefore(document.querySelector("#aboutArticle h2"), document.querySelector("#wrapper").firstChild);}
+const addHeader2 = function () { document.querySelector("#wrapper").insertBefore(document.querySelector("#aboutArticle h2"), document.querySelector("#wrapper").firstChild); }
 
 //REMOVE ABOUT HEADER + PREPEND TO ABOUT ARTICLE AT >= 992PX
-window.addEventListener ("resize",
+window.addEventListener("resize",
   function () {
     if (window.innerWidth >= 992 || document.documentElement.clientWidth >= 992) {
-      getRid ();
-      addHeader ();
+      getRid();
+      addHeader();
     }
     else {
-      addHeader2 ();
+      addHeader2();
     }
   }, false
 );
 
-window.addEventListener ("load",
+window.addEventListener("load",
   function () {
     if (window.innerWidth >= 992 || document.documentElement.clientWidth >= 992) {
-      getRid ();
-      addHeader ();
+      getRid();
+      addHeader();
     }
   }, false
 );
